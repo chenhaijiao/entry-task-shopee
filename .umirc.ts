730f37c5df596039bdba7f11e5927254d90e154f
haijiao.chen <haijiao.chen@shopee.com>
2026-04-09 20:57:15 +0800

    修复打包后public文件不存在问题

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@umijs/max';
 import px2rem from 'postcss-pxtorem';
 export default defineConfig({
+  outputPath: 'public',
   npmClient: 'yarn',
   history: { type: 'browser' },
   locale: {
